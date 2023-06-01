e2a845974098ffd831beb16aefae190c05043979
117503445 <t117503445@gmail.com>
2023-06-01 15:47:37 +0800

    chore: mypy
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- script/write_git_info.sh
+++ script/write_git_info.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# 获取最近的标签名称，如果不存在则返回空字符串
-tag=$(git describe --tags --abbrev=0 HEAD 2>/dev/null || echo "")
-
+tag=$(git tag --points-at HEAD | sort -V | tail -n 1)
 if [ -n "$tag" ]; then
-  # 如果存在标签，则写入标签名称
-  echo -n "$tag" > ./flow-pdf/git.txt
+  echo -n "$tag" > ./flow_pdf/git.txt
 else
-  # 如果不存在标签，则写入提交哈希值
   commit_hash=$(git rev-parse HEAD)
-  echo -n "dev-$commit_hash" > ./flow-pdf/git.txt
+  echo -n "dev-$commit_hash" > ./flow_pdf/git.txt
 fi
