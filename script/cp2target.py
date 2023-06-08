51305311410566239cdd52a21bd7e7ba047fed36
117503445 <t117503445@gmail.com>
2023-06-09 00:24:10 +0800

    feat: cp2target with create time
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- script/cp2target.py
+++ script/cp2target.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import shutil
+from htutil import file
+from datetime import datetime
+
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
 
 dir_target = Path('./data') / 'target'
 dir_target.mkdir(parents=True, exist_ok=True)
@@ -8,7 +12,9 @@ dir_output =  Path('./data') / 'out'
 for f in dir_output.glob('*'):
     file_id = f / 'big_blocks_id.json'
     dir_dest = dir_target / f.name / 'big_blocks_id'
-    dir_dest.mkdir(parents=True, exist_ok=True)
+    if not dir_dest.exists():
+        dir_dest.mkdir(parents=True)
+        file.write_text(dir_dest / 'note.txt', f'created time: {current_time}\n\n')
 
     if file_id.exists():
         shutil.copy(file_id, dir_dest)
