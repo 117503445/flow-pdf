e4e3547f65b747a2eed78b6daa63cdad1a4193b4
117503445 <t117503445@gmail.com>
2023-06-09 15:26:48 +0800

    ci: fix write_git_info
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- script/write_git_info.sh
+++ script/write_git_info.sh
@@ -5,8 +5,7 @@ if [ -n "$tag" ]; then
   echo -n "$tag" > ./flow_pdf/git.txt
 else
   commit_hash=$(git rev-parse HEAD)
-  if [ -n "$commit_hash"] ; then
-  else
+  if [ ! -n "$commit_hash"] ; then
     commit_hash=$sha
   fi
   echo -n "dev-$commit_hash" > ./flow_pdf/git.txt
