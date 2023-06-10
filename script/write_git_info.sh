ca6de03fc164043bb8ab38a1bbf3716aaa683281
117503445 <t117503445@gmail.com>
2023-06-10 21:14:23 +0800

    fix: write git info
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- script/write_git_info.sh
+++ script/write_git_info.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -evx
+
 tag=$(git tag --points-at HEAD | sort -V | tail -n 1)
 if [ -n "$tag" ]; then
   echo -n "$tag" > ./git.txt
 else
   commit_hash=$(git rev-parse HEAD)
-  if [ ! -n "$commit_hash"] ; then
+  if [ ! -n "$commit_hash" ] ; then
     commit_hash=$sha
   fi
   echo -n "$commit_hash" > ./git.txt
