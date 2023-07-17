149a3015acac5c63300fd0112fbe41eebd21eb18
117503445 <t117503445@gmail.com>
2023-07-18 01:11:53 +0800

    feat: layout-parser demo
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ experiment/layout-parser/download.py
@@ -0,0 +1,7 @@
+import layoutparser as lp
+
+model = lp.Detectron2LayoutModel(
+            config_path ='lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config', # In model catalog
+            label_map   ={0: "Text", 1: "Title", 2: "List", 3:"Table", 4:"Figure"}, # In model`label_map`
+            extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8] # Optional
+        )
