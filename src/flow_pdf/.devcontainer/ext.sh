396759b4e85b6dc1847c3c6ab01bb7eaeaa6ebd6
117503445 <t117503445@gmail.com>
2023-07-25 23:21:22 +0800

    feat: flow_pdf dev container
    Signed-off-by: 117503445 <t117503445@gmail.com>

---
+++ src/flow_pdf/.devcontainer/ext.sh
@@ -0,0 +1,2 @@
+EXT_LIST="ms-python.python humao.rest-client github.copilot christian-kohler.path-intellisense esbenp.prettier-vscode redhat.vscode-yaml matangover.mypy donjayamanne.python-environment-manager ms-python.vscode-pylance" && \
+    for EXT in $EXT_LIST; do code --install-extension $EXT; done
