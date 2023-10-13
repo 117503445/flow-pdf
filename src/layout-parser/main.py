feb75384ec9be19200432d164e01773f28b20817
117503445 <t117503445@gmail.com>
2023-10-13 19:00:11 +0800

    feat: layout parser func
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/layout-parser/main.py
+++ src/layout-parser/main.py
@@ -10,10 +10,12 @@ dir_input = dir_data / 'input'
 model = lp.Detectron2LayoutModel(
             config_path ='lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config', # In model catalog
             label_map   ={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"}, # In model`label_map`
-            extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8] # Optional
+            extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.6] # Optional
         )
 
-for f in sorted(dir_input.glob('*.pdf')):
+CACHE = True
+
+def parse(f: Path):
     print(f)
     dir_output = dir_data / 'layout-parser-output' / f.stem
     dir_output.mkdir(parents=True, exist_ok=True)
@@ -32,7 +34,7 @@ for f in sorted(dir_input.glob('*.pdf')):
             f_layout = dir_output / "layout" / f"{i}.json"
             f_marked_img = dir_output / "marked" / f"{i}.png"
 
-            if not f_layout.exists() or not f_marked_img.exists():
+            if CACHE and not f_layout.exists() or not f_marked_img.exists():
                 image = cv2.imread(str(f_raw_img.absolute()))
                 layout = model.detect(image)
                 
@@ -40,5 +42,10 @@ for f in sorted(dir_input.glob('*.pdf')):
                 with open(f_layout, 'w') as f:
                     f.write(json.dumps(layout.to_dict(), indent=4))
 
-                im = lp.draw_box(image, layout)
+                im = lp.draw_box(image, layout, show_element_id = True, show_element_type = True)
                 im.save(f_marked_img)
+
+# parse(Path('data/input/HotStuff.pdf'))
+
+for f in sorted(dir_input.glob('*.pdf')):
+    parse(f)
