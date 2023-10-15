02cbd7a0474a5672860129f7671e60c3ec869b60
117503445 <t117503445@gmail.com>
2023-10-16 01:16:21 +0800

    feat: parse json
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/fc-local-dev/src/main.rs
+++ src/fc-local-dev/src/main.rs
@@ -2,12 +2,43 @@ use notify::{
     event::{AccessKind, AccessMode},
     Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher,
 };
+use serde::Deserialize;
+use std::io::Read;
 use std::path::Path;
+use std::{collections::HashMap, fs::File};
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct Trigger {
+    prefix: String,
+    suffix: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct Action {
+    r#type: String,
+    args: HashMap<String, String>,
+}
+
+#[derive(Deserialize, Debug)]
+#[allow(dead_code)]
+struct Rule {
+    triggers: Vec<Trigger>,
+    actions: Vec<Action>,
+}
 
 fn main() {
     // let path = std::env::args()
     //     .nth(1)
     //     .expect("Argument 1 needs to be a path");
+
+    let mut file = File::open("rule.json").expect("Failed to open rule.json");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read rule.json");
+    let rule: Rule = serde_json::from_str(&contents).expect("Failed to parse JSON");
+    println!("rule: {:?}", rule);
 
     let path = "/data";
 
