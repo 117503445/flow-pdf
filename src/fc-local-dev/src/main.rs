607dd2257310b11bfdddb48b3f15cffdd5c62e1c
117503445 <t117503445@gmail.com>
2023-10-15 23:45:58 +0800

    feat: notify
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- src/fc-local-dev/src/main.rs
+++ src/fc-local-dev/src/main.rs
@@ -1,3 +1,35 @@
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = std::env::args()
+        .nth(1)
+        .expect("Argument 1 needs to be a path");
+
+    println!("Watching {path}");
+
+    if let Err(error) = watch(path) {
+        println!("Error: {error:?}");
+    }
+}
+
+fn watch<P: AsRef<Path>>(path: P) -> notify::Result<()> {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
+
+    for res in rx {
+        match res {
+            Ok(event) => println!("Change: {event:?}"),
+            Err(error) => println!("Error: {error:?}"),
+        }
+    }
+
+    Ok(())
 }
